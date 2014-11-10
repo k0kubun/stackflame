f27a358f5fceb88935e26e714c01eb62244e4f6e
Takashi Kokubun <takashikkbn@gmail.com>
2014-11-10 22:00:00 +0900

    Complete implementation

--- lib/stackflame.rb
+++ lib/stackflame.rb
@@ -5,9 +5,39 @@ class Stackflame
   def self.profile(mode: :cpu, &block)
     stackflame = self.new
     stackflame.run(mode, &block)
+    stackflame.open_flamegraph
   end
 
   def run(mode, &block)
     result = StackProf.run(mode: mode, raw: true, &block)
+
+    File.open(temp_js_path, 'w') do |f|
+      StackProf::Report.new(result).print_flamegraph(f)
+    end
+  end
+
+  def open_flamegraph
+    if system("which osascript > /dev/null")
+      # NOTE: `open` can't open path with query string
+      `osascript -e 'open location "#{flamegraph_path}"'`
+    else
+      puts "This OS is not supported. Please open: #{flamegraph_path}"
+    end
+  end
+
+  private
+
+  def flamegraph_path
+    viewer_path = File.expand_path('../../vendor/viewer.html', __FILE__)
+    "file://#{viewer_path}\?data=#{temp_js_path}"
+  end
+
+  def temp_js_path
+    return @js_path if @js_path
+
+    temp = `mktemp /tmp/stackflame-XXXXXXXX`.strip
+    @js_path = "#{temp}.js"
+    `mv #{temp} #{@js_path}`
+    @js_path
   end
 end
